3145c4d70c47fc09d8b5f7cda5495aaed71670d2
Mayank Mali <mmali@andrew.cmu.edu>
2021-11-17 02:12:38 +0000

    convert horse.bin to png

--- examples/binary_file_parser.py
+++ examples/binary_file_parser.py
@@ -15,6 +15,15 @@
 import struct
 from struct import unpack
 
+import numpy as np
+import cairocffi as cairo
+
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+HORSE_FILEPATH = '../data/horse.bin'
+OUTPUT_SIZE = 256
 
 def unpack_drawing(file_handle):
     key_id, = unpack('Q', file_handle.read(8))
@@ -47,7 +56,71 @@ def unpack_drawings(filename):
             except struct.error:
                 break
 
+                
+def vector_to_raster(vector_images, side=28, line_diameter=16, padding=16, bg_color=(0,0,0), fg_color=(1,1,1), lim = -1):
+    """
+    padding and line_diameter are relative to the original 256x256 image.
+    """
+    
+    original_side = 256.
+    
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, side, side)
+    ctx = cairo.Context(surface)
+    ctx.set_antialias(cairo.ANTIALIAS_BEST)
+    ctx.set_line_cap(cairo.LINE_CAP_ROUND)
+    ctx.set_line_join(cairo.LINE_JOIN_ROUND)
+    ctx.set_line_width(line_diameter)
 
-for drawing in unpack_drawings('nose.bin'):
-    # do something with the drawing
-    print(drawing['country_code'])
+    # scale to match the new size
+    # add padding at the edges for the line_diameter
+    # and add additional padding to account for antialiasing
+    total_padding = padding * 2. + line_diameter
+    new_scale = float(side) / float(original_side + total_padding)
+    ctx.scale(new_scale, new_scale)
+    ctx.translate(total_padding / 2., total_padding / 2.)
+
+    raster_images = []
+    limit = lim
+    for vector_image in tqdm(vector_images):
+        # clear background
+        ctx.set_source_rgb(*bg_color)
+        ctx.paint()
+        
+        bbox = np.hstack(vector_image).max(axis=1)
+        offset = ((original_side, original_side) - bbox) / 2.
+        offset = offset.reshape(-1,1)
+        centered = [stroke + offset for stroke in vector_image]
+
+        # draw strokes, this is the most cpu-intensive part
+        ctx.set_source_rgb(*fg_color)        
+        for xv, yv in centered:
+            ctx.move_to(xv[0], yv[0])
+            for x, y in zip(xv, yv):
+                ctx.line_to(x, y)
+            ctx.stroke()
+
+        data = surface.get_data()
+        raster_image = np.copy(np.asarray(data)[::4])
+        raster_images.append(raster_image)
+        limit -= 1
+        if (limit == 0):
+            break
+    
+    return raster_images
+        
+if __name__ == '__main__':
+    
+    vector_images = map(lambda x: x['image'], unpack_drawings(HORSE_FILEPATH))
+    raster_images = vector_to_raster(vector_images, side=OUTPUT_SIZE, line_diameter=4, padding=16, bg_color=(1,1,1), fg_color=(0,0,0), lim = 10)
+    print('DONE: ', len(raster_images), type(raster_images[0]))
+    
+    name_template = "img_{:05d}.png"
+    for i, image in enumerate(raster_images):
+        plt.imsave(name_template.format(i), image.reshape((OUTPUT_SIZE,OUTPUT_SIZE)), cmap='gray')
+    
+    '''
+    for drawing in tqdm(unpack_drawings(HORSE_FILEPATH)):
+        # do something with the drawing
+
+        print(drawing['image'])
+    '''
